176a2b2c6d16e90e2cb9c2f3fd2ecde1bb108221
Julio Reyes <nemesiscodex@gmail.com>
2025-11-08 00:43:32 -0300

    initial commit

---
+++ src/main/java/org/nemesiscodex/transfers/TransfersApplication.java
@@ -0,0 +1,13 @@
+package org.nemesiscodex.transfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfersApplication.class, args);
+	}
+
+}
